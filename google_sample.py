0af3ad039f75723899278159875535fe2b6dd64b
Clovis Borges Nunes <cborgesn@everis.com>
2021-07-19 21:42:15 -0300

    ss counter

--- google_sample.py
+++ google_sample.py
@@ -7,6 +7,11 @@ from selenium.webdriver.support import expected_conditions as EC
 
 from selenium.common.exceptions import TimeoutException
 
+# TODO
+# Create dir for save screenshots
+# Generate pdf report
+# Make unit tests (unittest)
+
 chrome_options = Options()
 chrome_options.add_argument("--start-maximized")
 chrome_options.add_argument("--incognito")
@@ -25,8 +30,14 @@ def wait_get_by_xpath(xpath):
     
     return element
 
+n = 0
+def screen_counter():
+    global n
+    n += 1
+    return n
 
 wait_get_by_xpath("//img[@alt='Google']")
+driver.save_screenshot('screenshot' + str(screen_counter()) + '.png')
 
 field_search = driver.find_element_by_xpath("//input[@name='q']")
 
@@ -34,8 +45,10 @@ if(field_search.is_enabled() and field_search.is_displayed()):
     field_search.send_keys("python")
 
 option_list = wait_get_by_xpath("(//ul/li//span[text()='python'])[1]")
+driver.save_screenshot('screenshot' + str(screen_counter()) + '.png')
 option_list.click()
 
 python_result = wait_get_by_xpath("//h3[text()='Welcome to Python.org']")
+driver.save_screenshot('screenshot' + str(screen_counter()) + '.png')
 
 driver.quit()
